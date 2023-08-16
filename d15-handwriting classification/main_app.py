c4c28a896f7c38de3be013fd2bca70b299727162
dvrvsimi <daraakojede01gmail.com>
2023-08-16 12:58:45 +0100

    fixing script

--- d15-handwriting classification/main_app.py
+++ d15-handwriting classification/main_app.py
@@ -2,7 +2,7 @@ import pygame, sys
 from pygame.locals import *
 import numpy as np
 from keras.models import load_model
-# import cv2
+import cv2
 
 x_size = 720
 y_size = 480
@@ -26,9 +26,14 @@ displaySurface = pygame.display.set_mode((x_size, y_size))
 
 pygame.display.set_caption('handwriting board')
 
+
+font = pygame.font.SysFont('Arial', 30)
+boundInc = 10
 isWriting = False
 x_move_value = []
 y_move_value = []
+img_count = 1
+predict = True
 
 while True:
 
@@ -50,3 +55,49 @@ while True:
             isWriting = False
             x_move_value = sorted(x_move_value)
             y_move_value = sorted(y_move_value)
+
+        if event.type == MOUSEBUTTONDOWN:
+            isWriting = True
+
+        if event.type == MOUSEBUTTONUP:
+            isWriting = False
+            x_move_value = sorted(x_move_value)
+            y_move_value = sorted(y_move_value)
+
+            x_min, x_max = max(0, x_move_value[0]-boundInc), min(x_size, x_move_value[-1]+boundInc)
+            y_min, y_max = max(0, y_move_value[0]-boundInc), min(y_size, y_move_value[-1]+boundInc)
+            
+            x_move_value = []
+            y_move_value = []
+
+            image_array = np.array(pygame.PixelArray(displaySurface))[x_min:x_max, y_min:y_max].T.astype(np.float32)
+
+            if image_save:
+                cv2.imwrite('image.png')
+                img_count += 1
+            
+            if predict:
+                image_array = cv2.resize(image_array, (28, 28))
+                image_array = np.pad(image_array, (10, 10), 'constant', constant_values=0)
+                # image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2GRAY)
+                # ValueError, cannot reshape array of size 2304 into shape (1,28,28,1), what to do?
+                image_array = cv2.resize(image_array, (28, 28))/255
+
+                label = str(labels[np.argmax(model.predict(image_array.reshape(1, 28, 28, 1)))])
+
+                textSurface = font.render(label, True, red)
+                textRect = textSurface.get_rect()
+                # note if error shows from using x_min, y_max, use x_min, y_min
+                textRect.left, textRect.bottom = x_min, y_max
+
+                displaySurface.blit(textSurface, textRect)
+
+                prediction = model.predict(image_array)
+                print(str(labels[np.argmax(prediction)]))
+            
+            if event.type == KEYDOWN:
+                if event.unicode == 'n':
+                    displaySurface.fill(white)
+                                    
+    
+    pygame.display.update()
